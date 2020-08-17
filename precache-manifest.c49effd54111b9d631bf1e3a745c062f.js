5a76bf62bd95a4f31321286fafca825b431a61e4
guzhongren <guzhongren@live.cn>
2020-08-17 12:33:21 +0000

    deploy: f05cdb040b88d8af939c4b06b8441ddf33cb9e76

---
+++ precache-manifest.c49effd54111b9d631bf1e3a745c062f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5610d1913fef3cb29230eed4141bc31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5be83bd4e7a05759a7c5",
+    "url": "/static/css/2.0b51dc58.chunk.css"
+  },
+  {
+    "revision": "9d3b183f65c84760919d",
+    "url": "/static/css/main.aa3ead89.chunk.css"
+  },
+  {
+    "revision": "5be83bd4e7a05759a7c5",
+    "url": "/static/js/2.dfb84f93.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dfb84f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3b183f65c84760919d",
+    "url": "/static/js/main.92831978.chunk.js"
+  },
+  {
+    "revision": "b15e4ad6314b411a43ea",
+    "url": "/static/js/runtime-main.1e4db1fe.js"
+  }
+]);
